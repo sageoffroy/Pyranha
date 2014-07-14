81b3c0ba020c4d1d77311b145cbc02ce04427f30
fede <amandifederico@gmail.com>
2014-07-14 09:09:19 -0300

    Instalador de librerias version 1

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Instalando librerias"
+apt-get install python-pip
+apt-get install python-opencv
+apt-get install python-pyaudio
+pip install pyglet
+echo "Finalizando Instalacion"
