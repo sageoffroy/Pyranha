946d8c68f4c2a410fe5fac2d2b6d6fd0190026c4
fede <amandifederico@gmail.com>
2014-08-11 18:13:08 -0300

    Actualizado el install

--- install/install.sh
+++ install/install.sh
@@ -4,4 +4,9 @@ apt-get install python-pip
 apt-get install python-opencv
 apt-get install python-pyaudio
 pip install pyglet
+apt-get install python-xlib
+cd PyUserInput-master
+python setup.py build
+python setup.py install
+cd ..
 echo "Finalizando Instalacion"
