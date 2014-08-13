f9fb8fb0f9363071cea8c58f2368f5bcb4fa2212
Fede amandi <amandifederico@gmail.com>
2014-08-13 10:35:26 -0300

    install mejorado

--- install/install.sh
+++ install/install.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 echo "Instalando librerias"
-apt-get install python-pip
-apt-get install python-opencv
-apt-get install python-pyaudio
+apt-get install python-pip python-opencv python-qt4 python-pyaudio python-requests python-numpy python-xlib
+echo 's'
 pip install pyglet
-apt-get install python-xlib
+
 cd PyUserInput-master
 python setup.py build
 python setup.py install
