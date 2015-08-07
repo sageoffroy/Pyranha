f03bf555c79e0d0b0a2b089af00e49108fcc3324
Fede amandi <amandifederico@gmail.com>
2015-08-07 10:42:15 -0300

    sqlite corregido

--- install/install.sh
+++ install/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Instalando librerias"
-apt-get install python-pip python-opencv python-qt4 python-pyaudio python-requests python-numpy python-xlib
+apt-get install python-pip python-opencv python-qt4 python-pyaudio python-requests python-numpy python-xlib sqlite3
 echo 's'
 pip install pyglet
 
